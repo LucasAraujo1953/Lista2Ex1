46ca6f6927bb2e5b1f2afc30b91e6a3f8fc28f04
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-19 20:36:14 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.WriteLine("De dois valores distintos: ");
+            Console.Write("Digite o primeiro valor: ");
+            a=int.Parse(Console.ReadLine());
+            Console.Write("Digite o segundo valor: ");
+            b=int.Parse(Console.ReadLine());
+            if (a > b)
+                Console.WriteLine("O primeiro valor é maior que o Segundo.");
+               else
+                Console.WriteLine("O segundo valor é maior que o Primeiro.");
+        }
+    }
+}
